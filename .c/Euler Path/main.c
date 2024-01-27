ab3387afd9decdb8dece75d85cc0be4290b5e21a
Marco <marcosteam7@gmail.com>
2024-01-26 23:32:45 -0500

    Pretty format print

--- .c/Euler Path/main.c
+++ .c/Euler Path/main.c
@@ -2,15 +2,16 @@
 
 #define NUM_NODES 4
 
-void print(short adjmat[NUM_NODES][NUM_NODES]);
+void print(short mat[NUM_NODES][NUM_NODES]);
 
 
 int main() {
     short adjmat[NUM_NODES][NUM_NODES] = {
-        {0, 1, 0, 1},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1},
-        {1, 0, 0, 0}
+        /*0  1  2  3*/
+    /*0*/{0, 1, 0, 0},
+    /*1*/{0, 0, 1, 0},
+    /*2*/{0, 0, 0, 1},
+    /*3*/{1, 0, 0, 0}
     };
 
     print(adjmat);
@@ -18,10 +19,18 @@ int main() {
     return 0;
 }
 
-void print(short adjmat[NUM_NODES][NUM_NODES]) {
+void print(short mat[NUM_NODES][NUM_NODES]) {
+
+    printf("    ");
+    for (int i = 0; i < NUM_NODES; i++) { printf("%d ", i); } 
+    printf("\n   ");
+    for (int i = 0; i < NUM_NODES*2; i++) { printf("-"); } 
+    printf("\n");
+
     for (int x = 0; x < NUM_NODES; x++) {
+        printf("%d | ", x);
         for (int y = 0; y < NUM_NODES; y++) {
-            printf("%d ", adjmat[x][y]);
+            printf("%d ", mat[x][y]);
         }
         printf("\n");
     }
