3ab628624d5e46a47039bc9e382b7ce3b33932e8
Marco <marcosteam7@gmail.com>
2024-01-27 11:58:34 -0500

    dfs template

--- .c/Euler Path/main.c
+++ .c/Euler Path/main.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-#define NUM_NODES 4
+#define NUM_NODES 6
+#define MAX_NODES 15
 
-void print(short mat[NUM_NODES][NUM_NODES]);
+void dfs(short adjmat[NUM_NODES][NUM_NODES], int node, bool visited[NUM_NODES]);
+void print(short adjmat[NUM_NODES][NUM_NODES]);
 
 
 int main() {
     short adjmat[NUM_NODES][NUM_NODES] = {
-        /*0  1  2  3*/
-    /*0*/{0, 1, 0, 0},
-    /*1*/{0, 0, 1, 0},
-    /*2*/{0, 0, 0, 1},
-    /*3*/{1, 0, 0, 0}
-    };
+        /*0  1  2  3  4  5*/
+    /*0*/{0, 1, 1, 0, 1, 1},
+    /*1*/{1, 0, 1, 0, 1, 1},
+    /*2*/{1, 1, 0, 1, 0, 1},
+    /*3*/{0, 0, 1, 0, 1, 0},
+    /*4*/{1, 1, 0, 1, 0, 1},
+    /*5*/{1, 1, 1, 0, 1, 0}};
 
-    print(adjmat);
+    bool visited[NUM_NODES] = {false};
+
+    dfs(adjmat, 0, visited);
 
     return 0;
+}
+
+void dfs(short adjmat[NUM_NODES][NUM_NODES], int node, bool visited[NUM_NODES]) {
+    printf("%d ", node);
+
+    visited[node] = true;
+
+    for (int i = 0; i < NUM_NODES; i++) {
+        if (adjmat[node][i] == 1 && !visited[i]) {
+            dfs(adjmat, i, visited);
+        }
+    }
 }
 
 void print(short mat[NUM_NODES][NUM_NODES]) {
