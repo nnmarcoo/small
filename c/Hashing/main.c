9b3190bf93a92958667796780f3e480b60a08656
Marco <marcosteam7@gmail.com>
2024-03-07 23:39:49 -0500

    Fills string in with zeros

--- c/Hashing/main.c
+++ c/Hashing/main.c
@@ -8,19 +8,17 @@ inline unsigned int padlength(const char* str) { return ((strlen(str)*8 + 8 + 64
 
 int main(void) {
     
-    printf("%d\n", md5("helloworld"));
-    
+    md5("helloworld"); 
     return 0;
 }
 
 unsigned int md5(const char* str) {
     unsigned int length = padlength(str);
-    unsigned int zeros = length - strlen(str) - 1;
-    char* paddedstr = (char*)malloc(length);
-    strcpy(paddedstr, str);
-    strcat(paddedstr, "1");
-    
-    //printf("%s\n", paddedstr);
-    return strlen(paddedstr);
+    char* paddedstr = (char*)malloc(length + 1);
+    memset(paddedstr, '0', length);
+    paddedstr[length] = '\0';        // Is this necessary?
+    printf("%s\n", paddedstr);
+    free(paddedstr);
+    return 0;
 }
 
