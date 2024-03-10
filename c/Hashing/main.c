80181194c5bbfb88bbedc9d1934415d4ff1532a6
Marco <marcosteam7@gmail.com>
2024-03-10 01:29:36 -0500

    Fixed signed char wrap issue

--- c/Hashing/main.c
+++ c/Hashing/main.c
@@ -4,8 +4,8 @@
 #include <math.h>
 
 unsigned int padlength(unsigned int length) { return (((length*8 + 8 + 64 + 512 - 1) / 512) * 512) / 8; }
-unsigned int md5(const char* str);
-void print(char* str, unsigned int len);
+unsigned int md5(unsigned char* str);
+void print(unsigned char* str, unsigned int len);
 
 int main(void) {
     
@@ -13,10 +13,11 @@ int main(void) {
     return 0;
 }
 
-unsigned int md5(const char* str) {
+unsigned int md5(unsigned char* str) {
     unsigned int strlength = strlen(str);
     unsigned int blocklength = padlength(strlength);
-    char* paddedstr = (char*)malloc(blocklength);
+    unsigned char* paddedstr = (unsigned char*)malloc(blocklength);
+
 
     strcpy(paddedstr, str);
     paddedstr[strlength] = 0x80;
@@ -28,12 +29,12 @@ unsigned int md5(const char* str) {
     return 0;
 }
 
-void print(char *str, unsigned int len) {
+void print(unsigned char *str, unsigned int len) {
     for (unsigned int i = 0; i < len; i++) {
-        if (str[i] == 0)
-            printf("0");
-        else if (str[i] == 0x80)
+        if (str[i] == 0x80)
             printf("1");
+        else if (str[i] == 0)
+            printf("0");
         else
             printf("%c", str[i]);
     }
